e3ff2c0db33bb3ea49c676533518f27e03ac7e2d
Andres <andrescn20@gmail.com>
2022-04-07 12:04:18 -0600

    playRound determines round winner according to inputs

--- script.js
+++ script.js
@@ -11,4 +11,33 @@ function computerSelection(){
         return "Scissors";
     }
 }
+console.log(computerSelection())
+function playRound ( computerSelection , playerSelection ) {
+    if (computerSelection === playerSelection ){
+        return "Computer choose "+computerSelection+ ". You chose "+playerSelection+". Its a Draw"
 
+    }else{
+        switch (computerSelection) {
+            case ("Paper"):
+                if ( playerSelection === "Rock" ){
+                    return "Computer chose Paper. You chose Rock. Computer wins this round."
+
+                }else if (playerSelection === "Scissors"){
+                    return "Computer chose Paper. You chose Scissors. You win this round."
+                }
+            case ("Scissors"):
+                if ( playerSelection === "Rock" ){
+                    return "Computer chose Scissors. You chose Rock. You win this round."
+                }else if (playerSelection === "Paper"){
+                    return "Computer chose Scissors. You chose Scissors. Computer wins this round."
+                }
+            case ("Rock"):
+                if (playerSelection === "Scissors" ){
+                    return "Computer chose Rock. You Chose Scissors. Computer wins this round"
+                }else if (playerSelection === "Paper"){
+                    return "Computer chose Rock. you Chose Paper. You win this round"
+                }     
+         }
+    }
+}
+        
