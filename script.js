1a66e74350814d5b650bd3a5d55a0b809f876f1a
Andres <andrescn20@gmail.com>
2022-04-07 18:39:26 -0600

    Fix wrong placed variables. Fix Global counter and final text selector

--- script.js
+++ script.js
@@ -12,78 +12,80 @@ function computerSelection(){
         return "Scissors";
     }
 }
-
+let x = 0;
 /*Receives selections from computer and player. Evaluates who won according to game rules. Lastly, 
 shows a message declaring the winner of the round*/
 
 function playRound ( computerSelection , playerSelection ) {
     let winnerText = "Its a Draw.";
-    let x = 0;
-
+    let win = x;
     if (computerSelection === playerSelection ){
-        
+        x = 0;
     }else{
         switch (computerSelection) {
 
             case ("Paper"):
                 if ( playerSelection === "Rock" ){
-                    x = 1;              
+                    win = 1;
+
                 }else if (playerSelection === "Scissors"){
-                    x = 2; 
+                    win = 2; 
                 }
+                break;
 
             case ("Scissors"):
                 if ( playerSelection === "Rock" ){
-                    x = 2; 
-                }else if (playerSelection === "Paper"){
-                    x = 1;
-                }
+                    win = 2; 
 
+                }else if (playerSelection === "Paper"){
+                    win = 1;
+                }
+                break;
             case ("Rock"):
                 if (playerSelection === "Scissors" ){
-                    x = 1;
+                    win = 1;
                 }else if (playerSelection === "Paper"){
-                    x = 2; 
-                }     
-         }  
-            if (x === 1)       { winnerText = "Computer wins this Round."}
-            else               { winnerText = "You win this Round."}
+                    win = 2; 
+                }
+                break;     
+            }
+            
     }
-
+    if (win === 1)       { winnerText = "Computer wins this Round."}
+    else if (win ===2)             { winnerText = "You win this Round."}
+    x = win;
     console.log("Computer chose "+computerSelection+" . You chose "+playerSelection+". "+winnerText);
-    return x;
 }
 
 /*Main Function for gameplay. Loops 5 rounds and adds one point for every win. Shows global winner
 at the end.*/
-function game(n){
-
+function game(){
+    let n = window.prompt('Define how many rounds decide the winner.', '5');
     let playercount = 0;
     let computercount = 0;
-
+    
     for (i=0; i < n; i++) {
-        
+           
         /*Ask User for Selection*/
-        playerSelection = window.prompt('Make your Choice: Rock, Paper or Scissors?', 'Rock, Paper or Scissors');
+        playerSelection = window.prompt('Round #'+(i+1)+'Make your Choice: Rock, Paper or Scissors?', 'Rock, Paper or Scissors');
 
         /*Round is played*/
-        let x = playRound(computerSelection(), playerSelection);
-        
-        switch (x) {
-            case (x === 0):
+        playRound(computerSelection(), playerSelection);
+
+        let point = x; 
+
+            if (point === 0){
             playercount = playercount;
             computercount = computercount;
-
-            case (x === 1):
+            } else if (point === 1){
                 playercount = playercount;
-                computercount = computercount++;
-
-            case (x === 2):
-                playercount = playercount++;
+                computercount = computercount + 1;
+            } else if (point === 2){
+                playercount = playercount +1;
                 computercount = computercount;
-
+            }
         }
-    }
+
     let winner = 0; 
     let globalWinnerText = "This Epic Showdown ends up as a Draw"
 
